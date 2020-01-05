60a61949d33768d2324b47f6b288d56c4b28b2b4
WinCubeML <mati031398@gmail.com>
2020-01-05 17:27:03 +0100

    Part II: Uploading files works!

--- src/main/java/pl/pw/pamiw/biblio/controller/FileController.java
+++ src/main/java/pl/pw/pamiw/biblio/controller/FileController.java
@@ -1,20 +1,27 @@
 package pl.pw.pamiw.biblio.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 import pl.pw.pamiw.biblio.exceptions.ExpiredSessionException;
 import pl.pw.pamiw.biblio.exceptions.ForbiddenCookieException;
 import pl.pw.pamiw.biblio.model.SessionData;
+import pl.pw.pamiw.biblio.model.UserForFileDTO;
 import pl.pw.pamiw.biblio.service.FileService;
 import pl.pw.pamiw.biblio.service.LoginService;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Arrays;
 
 @Controller
@@ -63,11 +70,11 @@ public class FileController { // TODO zrobić kontroler do plików
     }
 
     @RequestMapping(value = "/files", method = RequestMethod.GET)
-    public String getFilePage(HttpServletRequest request, HttpServletResponse response) {
+    public String getFilePage(HttpServletRequest request, HttpServletResponse response, Model model) {
         ResponseEntity responseEntity = checkCookies(request, response);
         if (responseEntity.getStatusCode().equals(HttpStatus.OK)) {
             //TODO obsługa JWT listowania
-            //TODO wyświetlanie listowania plików
+            model.addAttribute("files", fileService.listAllFiles());
             return "files";
         } else {
             return "forbidden";
@@ -75,14 +82,25 @@ public class FileController { // TODO zrobić kontroler do plików
     }
 
     @RequestMapping(value = "/files/upload", method = RequestMethod.POST)
-    public String uploadFile(HttpServletRequest request, HttpServletResponse response) {
+    public String uploadFile(HttpServletRequest request, HttpServletResponse response, @ModelAttribute MultipartFile file) {
         ResponseEntity responseEntity = checkCookies(request, response);
         if (responseEntity.getStatusCode().equals(HttpStatus.OK)) {
             //TODO obsługa JWT dodawania
-            //TODO obsługa dodawania plików
+            try {
+                fileService.uploadFile(new UserForFileDTO("test", "test"), file);
+            } catch (IOException e) {
+                System.out.println("error");
+                e.printStackTrace();
+                return "redirect:/files";
+            }
             return "redirect:/files";
         } else {
             return "forbidden";
         }
+    }
+
+    @RequestMapping(value = "/files/download/{fileName}", method = RequestMethod.GET)
+    public HttpEntity<byte[]> downloadFile(@PathVariable("fileName") String fileName) {
+        return null;
     }
 }
--- src/main/java/pl/pw/pamiw/biblio/model/UserForFileDTO.java
+++ src/main/java/pl/pw/pamiw/biblio/model/UserForFileDTO.java
@@ -1,8 +1,10 @@
 package pl.pw.pamiw.biblio.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class UserForFileDTO {
     private String name;
     private String surname;
